943906ab1586d68bcad0f2caa4d041c7edd8ee0a
jowen_wh <jake.owen@williamhill.com>
2022-04-10 17:37:10 +0100

    build

---
+++ build/precache-manifest.ec5065069225be7f4c2ee92234425c4a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6694611b82b15773ed4252c844e808ac",
+    "url": "/index.html"
+  },
+  {
+    "revision": "27eaaa3278eaa61a3e76",
+    "url": "/static/css/main.897227d1.chunk.css"
+  },
+  {
+    "revision": "338ba587d92a1d68c011",
+    "url": "/static/js/2.ae3b9a45.chunk.js"
+  },
+  {
+    "revision": "11df3877f8f22fee17e9340f0457334c",
+    "url": "/static/js/2.ae3b9a45.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "27eaaa3278eaa61a3e76",
+    "url": "/static/js/main.b94e6dc8.chunk.js"
+  },
+  {
+    "revision": "f35af6f7ccead2fe5dbb",
+    "url": "/static/js/runtime-main.4422d9b2.js"
+  }
+]);
