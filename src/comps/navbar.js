e4508b81848c306a0d8c173e6710aa85d045aae6
jowen_wh <jake.owen@williamhill.com>
2022-04-11 18:03:27 +0100

    nav bar and some content

---
+++ src/comps/navbar.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faTwitter, faDiscord, faInstagram, faLinkedin } from '@fortawesome/free-brands-svg-icons'
+
+
+
+const Navbar = () => {
+    return (
+        <nav className='navbar'>
+            <div className='projectName' style={{float: 'left' , padding: '5px'}}>Gainz Club</div>
+            <a href="https://twitter.com/GainzClubNFT"><FontAwesomeIcon icon={faTwitter} size="3x"/></a>
+            <a href="https://www.instagram.com/gainzclubnft/"><FontAwesomeIcon icon={faInstagram} size="3x"/></a>
+            <a href="https://discord.gg/eTYbrQvBTV"><FontAwesomeIcon icon={faDiscord} size="3x"/></a>
+            <a href="https://www.linkedin.com/in/jake-owen/"><FontAwesomeIcon icon={faLinkedin} size="3x"/></a>
+        </nav>
+    );
+};
+
+
+Navbar.propTypes = {
+
+};
+
+
+export default Navbar;
