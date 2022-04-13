aed256b3fffd1d05e485a33b83c29d656dd101f0
jowen_wh <jake.owen@williamhill.com>
2022-04-13 23:15:36 +0100

    fix

---
+++ build/precache-manifest.345ad24e05c3bbb7ed690f5159beb7ad.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8b6552c5f8a962d8d32ded1af27f80a4",
+    "url": "/index.html"
+  },
+  {
+    "revision": "45c40628960a9894aab7",
+    "url": "/static/css/main.fcf4db0f.chunk.css"
+  },
+  {
+    "revision": "464a631761c33dbb5f13",
+    "url": "/static/js/2.87bacd5b.chunk.js"
+  },
+  {
+    "revision": "dc555bc84ec4c4a9e921a85b57f41999",
+    "url": "/static/js/2.87bacd5b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "45c40628960a9894aab7",
+    "url": "/static/js/main.7e99d943.chunk.js"
+  },
+  {
+    "revision": "7b522822c949bd21083b",
+    "url": "/static/js/runtime-main.864dd752.js"
+  }
+]);
